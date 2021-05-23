9053a3ec3994eea5195adad1e1bda2ea442aa891
devu0409 <devanshu_gwl08@yahoo.com>
2021-05-24 00:02:28 +0530

    Added filter to get all tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,10 +22,18 @@ router.post('/tasks', auth, async (req, res) => {
 
 //get all tasks
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if(req.query.completed){
+        match.completed = req.query.completed === "true"
+    }
 
     try{
         //const tasks = await Task.find({owner:req.user._id})
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.status(200).send(req.user.tasks)
 
     }catch(e){
