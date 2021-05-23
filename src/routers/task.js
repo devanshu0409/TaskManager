927a3144c2fa04f72bc2f40d0f251cec8aca7254
devu0409 <devanshu_gwl08@yahoo.com>
2021-05-24 00:27:01 +0530

    Added sorting to getAllTasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,9 +23,15 @@ router.post('/tasks', auth, async (req, res) => {
 //get all tasks
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if(req.query.completed){
         match.completed = req.query.completed === "true"
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try{
@@ -35,7 +41,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.status(200).send(req.user.tasks)
